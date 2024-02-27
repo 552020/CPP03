3ae0de919b2b30c481bb96d079cbdc7f675e1784
552020 <stefanolombardo@posteo.de>
2024-02-27 14:58:08 +0100

    update and correct ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,5 +1,6 @@
 #include "ClapTrap.hpp"
 
+// It is not clear if we should have a default constructor or not
 ClapTrap::ClapTrap() : _name("Deffy"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
 	std::cout << "ClapTrap default constructor called" << std::endl;
@@ -41,9 +42,14 @@ ClapTrap::~ClapTrap()
 }
 void ClapTrap::attack(const std::string &target)
 {
+	// Of course, ClapTrap can’t do anything if it has no hit points
+	// or energy points left.
+	// We need to rewrite this properly; like this:
+	// if (_energyPoints <= 0 || _hitPoints <= 0)
 	if (_energyPoints <= 0)
 	{
-		std::cout << "ClapTrap " << _name << " is out of energy! Attack not possible!" << std::endl;
+		std::cout << "ClapTrap " << _name
+				  << " can't do anything: No hit points of energy points left! Attack not possible!" << std::endl;
 		return;
 	}
 	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!"
@@ -86,7 +92,7 @@ void ClapTrap::beRepaired(unsigned int amount)
 		std::cout << "ClapTrap " << _name << " cannot be repaired because it's destroyed!" << std::endl;
 		return;
 	}
-
+	// It's not stated that the hit points can't be more than 10
 	if (_hitPoints >= 10)
 	{
 		std::cout << "ClapTrap " << _name << " is already at full health!" << std::endl;
@@ -95,4 +101,38 @@ void ClapTrap::beRepaired(unsigned int amount)
 	_energyPoints--;
 	_hitPoints = _hitPoints + amount > 10 ? 10 : _hitPoints + amount;
 	std::cout << "ClapTrap " << _name << " has been repaired!" << std::endl;
+}
+
+// Getters and setters
+std::string ClapTrap::getName() const
+{
+	return _name;
+}
+void ClapTrap::setName(std::string name)
+{
+	_name = name;
+}
+int ClapTrap::getHitPoints() const
+{
+	return _hitPoints;
+}
+void ClapTrap::setHitPoints(int hitPoints)
+{
+	_hitPoints = hitPoints;
+}
+int ClapTrap::getEnergyPoints() const
+{
+	return _energyPoints;
+}
+void ClapTrap::setEnergyPoints(int energyPoints)
+{
+	_energyPoints = energyPoints;
+}
+int ClapTrap::getAttackDamage() const
+{
+	return _attackDamage;
+}
+void ClapTrap::setAttackDamage(int attackDamage)
+{
+	_attackDamage = attackDamage;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,20 +3,27 @@
 
 int main()
 {
+	std::cout << "ClapTrap clappy(\"Clappy\");" << std::endl;
 	ClapTrap clappy("Clappy");
+	std::cout << "\nClapTrap trappy(\"Trappy\");" << std::endl;
 	ClapTrap trappy("Trappy");
-
+	std::cout << "\nclappy.attack(\"Trappy\");" << std::endl;
 	clappy.attack("Trappy");
-
+	std::cout << "\ntrappy.takeDamage(20);" << std::endl;
 	trappy.takeDamage(20);
-
+	std::cout << "\ntrappy.beRepaired(10);" << std::endl;
 	trappy.beRepaired(10);
-
+	std::cout << "\nclappy.attack(\"ClapTrap_Two\");" << std::endl;
 	clappy.attack("ClapTrap_Two");
-
+	std::cout << "\nClapTrap fuffy = clappy;" << std::endl;
 	ClapTrap fuffy = clappy;
-
+	std::cout << "\nfuffy's name (the name of the object) 'fuffy' is " << fuffy.getName() << std::endl;
+	std::cout << "\nfuffy.setName(\"Fuffy\");" << std::endl;
+	fuffy.setName("Fuffy");
+	std::cout << "fuffy's name (the name of the fuffy object) is " << fuffy.getName() << std::endl;
+	std::cout << "\nfuffy.attack(\"Trappy\");" << std::endl;
 	fuffy.attack("Trappy");
+	// When Trappy is destroyed also Clanppy_Two is destroyed
 
 	return 0;
 }

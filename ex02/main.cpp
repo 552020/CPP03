1a5fb1b2ce0f473cbb96baf52dc4514b4dcd1885
552020 <stefanolombardo@posteo.de>
2024-02-28 19:39:52 +0100

    tweak main fraggy

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,22 +5,32 @@
 
 int main()
 {
+	std::cout << "***ClapTrap clappy(\"Clappy\");***" << std::endl;
 	ClapTrap clappy("Clappy");
+	std::cout << "\n***ScavTrap scavvy(\"Scavvy\");***" << std::endl;
 	ScavTrap scavvy("Scavvy");
+	std::cout << "\n***FragTrap fraggy(\"Fraggy\");***" << std::endl;
 	FragTrap fraggy("Fraggy");
 
 	std::cout << std::endl;
 
+	std::cout << "\n***clappy.attack(\"a target\");***" << std::endl;
 	clappy.attack("a target");
+	std::cout << "\n***scavvy.attack(\"another target\");***" << std::endl;
 	scavvy.attack("another target");
+	std::cout << "\n***fraggy.attack(\"yet another target\");***" << std::endl;
 	fraggy.attack("yet another target");
 
 	std::cout << std::endl;
 
+	std::cout << "\n***scavvy.guardGate();***\n";
 	scavvy.guardGate();
+	std::cout << "\n***fraggy.highFivesGuys();***\n";
 	fraggy.highFivesGuys();
 
 	std::cout << std::endl;
+
+	std::cout << "\n***Destructor for clappy, scavvy, and fraggy will be called now!***" << std::endl;
 
 	return 0;
 }
